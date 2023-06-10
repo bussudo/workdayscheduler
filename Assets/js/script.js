4707073872a2e760e49996feea4c303f9816e863
bussudo <105820926+bussudo@users.noreply.github.com>
2023-06-10 12:26:48 -0400

    completed design and functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,41 +1,43 @@
 // Selects element by class
-//var today = moment();
-//$("#currentday").text(today.format("DDDDD, MMMM Do"));
-var currentDay = moment(today).format('MMMM Do YYYY');
-// Selects element by id
-var mainEl = document.getElementById("main");
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+    var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+    localStorage.setItem(time, text);
+  });
 
-var secondsLeft = 30;
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-function setTime() {
-  // Sets interval in variableSU
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+  function hourTracker() {
+    var curHour = moment().hour();
+    $(".time-block").each(function (index, value) {
+      console.log($(value).attr("id").split("hour")[1]);
+      var blockHour = parseInt($(this).attr("id").split("hour")[1]);
 
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
-    }
-
-  }, 1000);
-}
-
-// Function to create and append colorsplosion image
-function sendMessage() {
-  timeEl.textContent = " ";
-  var imgEl = document.createElement("img");
-  imgEl.setAttribute("src", "images/image_1.jpg");
-  mainEl.appendChild(imgEl);
-
-}
-
-setTime();
-
-$(function() {  
-  $("#button").click(function() { 
-       $("#button").addClass("onclic", 250, validate); 
-       });
-      })
+      if (blockHour < curHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+      } else if (blockHour === curHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  hourTracker();
+});
